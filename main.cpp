0ad98cf02cd90f73ec8d08a50ec559bb13276d8b
Xiaoyu Qu <Xiaoyu.Qu@omnitek.online>
2022-03-11 11:38:11 +0100

    Return mutile results from one function

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using std::cin;
 using std::endl;
 using std::string;
 
+#define NrResults 4
 //return the start pointer of multiple results
 int *Compute(int n1, int n2,int *result){
 
@@ -17,17 +18,16 @@ return result;
 
 int main()
 {
-    int num1=15,num2=3;
+    int num1=15,num2=3,i;
     //int p[4];
     int *p;
     //it needs to allocate memory for this pointer first.
-    p=(int *)malloc(4*sizeof(int));
+    p=(int *)malloc(NrResults*sizeof(int));
 
     Compute(num1,num2,p);
-    cout<<"num1+num2="<<*(p)<<endl;
-    cout<<"num1-num2="<<*(p+1)<<endl;
-    cout<<"num1*num2="<<*(p+2)<<endl;
-    cout<<"num1/num2="<<*(p+3)<<endl;
+
+    for(i=0;i<NrResults;i++)
+        cout<<"result"<<i+1<<" is "<<*(p+i)<<endl;
 
     free(p);
     return 0;
